27d757b5bebca8fffd7a9d6d4366353549bf1dc2
omsatyawanpathakgit <omsatyawanpathakgit@gmail.com>
2026-08-29 16:58:20 +0530

    updated the cicd pipeline

--- BookRecommenderSystem_MLProject/book_recommender_flask/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-# Test package for Book Recommender Flask Application
--- BookRecommenderSystem_MLProject/book_recommender_flask/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Add parent directory to path for imports
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
